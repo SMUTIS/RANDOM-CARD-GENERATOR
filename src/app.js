6b0f856a9fb5726e512ad1455457bc548640554f
SMUTIS <sandramutissanabria@gmail.com>
2021-08-30 19:58:50 +0000

    inicial

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
+let suits = ["spade", "diamond", "heart", "club"];
+window.getCard = function getCard() {
+  let card = document.querySelector(".card");
+  for (let i = 0; i < suits.length; i++) {
+    if (card.classList.contains(suits[i])) {
+      card.classList.remove(suits[i]);
+    }
+  }
+  card.classList.add(generateRandomSuit(suits));
+  card.innerHTML = generateRandomNumber();
+};
+let generateRandomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+let generateRandomSuit = suits => {
+  let indexSuits = Math.floor(Math.random() * suits.length);
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  return suits[indexSuits];
 };
